33093394c608cee9f5f71091441d8254cbf0d052
Aimee Laws <aimee.laws13@gmail.com>
2023-09-12 14:24:33 +0100

    placeholder data

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import code from '@/public/code.png';
 import codeGear from '@/public/code-gear.png';
+import wires from '@/public/wires.png';
 
 const Skills = () => {
   return (
@@ -9,7 +10,7 @@ const Skills = () => {
         <div className="lg:mr-10 2xl:mr-28">
           <h1 className="m-0 text-4xl md:text-5xl 2xl:text-6xl">Skills</h1>
 
-          <p className="my-10 text-sm md:text-base 2xl:text-lg">
+          <p className="mt-5 text-sm md:text-base 2xl:text-lg">
             Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
             in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
             Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
@@ -33,6 +34,58 @@ const Skills = () => {
             className="hidden md:block lg:hidden 2xl:block"
           />
         </div>
+      </div>
+
+      <div className="mx-3 md:mx-10 xl:mx-32 2xl:mx-[20%] my-10 text-sm md:text-base 2xl:text-lg">
+        <h2 className="mb-2 text-lg md:text-xl 2xl:text-2xl font-bold">HTML and CSS</h2>
+        <p className="mt-0 mb-8">
+          Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
+          in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
+          Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
+        </p>
+
+        <h2 className="mb-2 text-lg md:text-xl 2xl:text-2xl font-bold">JavaScript and TypeScript</h2>
+        <p className="mt-0 mb-8">
+          Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
+          in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
+          Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
+        </p>
+      </div>
+
+      <Image src={wires} alt="group of wires graphic" className="my-10 w-full h-[12rem] object-cover" />
+
+      <div className="mx-3 md:mx-10 xl:mx-32 2xl:mx-[20%] my-10 text-sm md:text-base 2xl:text-lg">
+        <h2 className="mb-2 text-lg md:text-xl 2xl:text-2xl font-bold">React.js and Next.js</h2>
+        <p className="mt-0 mb-8">
+          Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
+          in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
+          Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
+        </p>
+
+        <h2 className="mb-2 text-lg md:text-xl 2xl:text-2xl font-bold">MongoDB</h2>
+        <p className="mt-0 mb-8">
+          Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
+          in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
+          Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
+        </p>
+      </div>
+
+      <Image src={wires} alt="group of wires graphic" className="my-10 w-full h-[12rem] object-cover" />
+
+      <div className="mx-3 md:mx-10 xl:mx-32 2xl:mx-[20%] mt-10 mb-20 text-sm md:text-base 2xl:text-lg">
+        <h2 className="mb-2 text-lg md:text-xl 2xl:text-2xl font-bold">TailwindCSS</h2>
+        <p className="mt-0 mb-8">
+          Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
+          in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
+          Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
+        </p>
+
+        <h2 className="mb-2 text-lg md:text-xl 2xl:text-2xl font-bold">Figma</h2>
+        <p className="mt-0 mb-8">
+          Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
+          in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
+          Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
+        </p>
       </div>
     </main>
   );
