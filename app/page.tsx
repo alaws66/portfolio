1b515b01ec188f44fd9dd4143e545a86256e6051
Aimee Laws <aimee.laws13@gmail.com>
2023-09-12 12:20:19 +0100

    style adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,12 @@ import wires from '@/public/wires.png';
 export default function Home() {
   return (
     <main>
-      <div className="flex mb-20 px-3 md:px-10 xl:px-32 2xl:px-[10%] py-10 bg-primary-50">
-        <div>
+      <div className="flex flex-col lg:flex-row items-center lg:items-start mb-20 px-3 md:px-10 xl:px-32 2xl:px-[10%] py-10 bg-primary-50">
+        <div className="mr-10 2xl:mr-28">
           <h1 className="m-0 text-4xl md:text-5xl 2xl:text-6xl">Aimee Laws</h1>
           <h2 className="m-0 sm:ml-8 text-2xl md:text-3xl 2xl:text-4xl text-primary-600">IT Software Technician</h2>
 
-          <p className="my-10 2xl:mr-20 text-sm md:text-base 2xl:text-lg">
+          <p className="my-10 text-sm md:text-base 2xl:text-lg">
             Proin nibh urna, fermentum vitae accumsan et, eleifend ut dolor. Ut efficitur rutrum libero,
             in bibendum leo lobortis vitae. Mauris tempus elementum ante, quis porta dolor pretium nec.
             Duis quis purus auctor, pulvinar nunc vel, luctus urna. Vestibulum.
@@ -39,7 +39,7 @@ export default function Home() {
             alt="purple laptop with data graphic"
             width={600}
             height={350}
-            className="hidden 2xl:block"
+            className="hidden md:block lg:hidden 2xl:block"
           />
         </div>
       </div>
